5192f56544a0ee85cdcc994bb7ccdf08bdcc0604
RanaJay3101 <ranajay206@gmail.com>
2024-09-07 19:04:48 +0530

    Beginning of JavaScript

---
+++ c3_JavaScript/script.js
@@ -0,0 +1,15 @@
+// alert("Welcome to JavaScriptCOde");
+console.log("Hey I am console");
+
+var a = prompt("Enetr a numberrrrrrr..");
+
+console.log(a);
+
+var b = confirm("DO you wanna know the world ...?")
+
+if(b){
+    console.log("yes",b);
+}
+else{
+    console.log(b);
+}
